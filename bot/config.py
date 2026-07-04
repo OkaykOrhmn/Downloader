7bb162997a7548f84954e0b1ab0b8fc02f3d58e8
OkaykOrhmn <kiainsta22@gmail.com>
2026-07-04 22:42:19 +0330

    fix config of bot.

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,6 @@
 class BotConfig:
-    def __init__(self, admin_ids, welcome_message) -> None:
+    def __init__(self, admin_ids, welcome_message, webhook_url, webhook_port) -> None:
         self.admin_ids = admin_ids
         self.welcome_message = welcome_message
+        self.webhook_url = webhook_url
+        self.webhook_port = webhook_port
